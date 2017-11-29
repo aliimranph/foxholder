90e42505eeb610f8d334e6f95b71b525cab4b99a
eisenfox <nat.eisen.davydova@gmail.com>
2017-11-29 16:07:27 +0300

    2.1.1: improved js code for most of the effects, improved structure of eff #10 and #15

--- src/foxholder.js
+++ src/foxholder.js
@@ -43,55 +43,47 @@ jQuery.fn.foxholder = function(number) {
 
   //examples scripts
 
-  if (number.demo == 2) {
-
-    //example-2 adding top property for label
-    jQuery('.foxholder-form__input--effect-2, .foxholder-form__textarea--effect-2').focus(function() {
-      var labelTop;
-      labelTop = parseInt(jQuery(this).css('padding-top'));
-      jQuery(this).next('label').css({'top': 0 - (labelTop + 7)});
-    });
-
-    jQuery('.foxholder-form__input--effect-2, .foxholder-form__textarea--effect-2').blur(function() {
-      if (! jQuery(this).hasClass('is-active')) {
-        jQuery(this).next('label').css({'top': 0});
-      } 
-    });
-  }
-
   if (number.demo == 3) {
 
     //example-3 paddings for inputs
-    jQuery('.foxholder-form__input--effect-3, .foxholder-form__textarea--effect-3').focus(function() {
-      var labelWidth;
+    jQuery('.foxholder-form__input--effect-3, .foxholder-form__textarea--effect-3').each(function() {
+      var labelWidth, initPadding;
       labelWidth = jQuery(this).siblings('label').width() + 36;
-      jQuery(this).css({'padding-left': labelWidth});
-    });
+      initPadding = jQuery(this).css('padding-left');
 
-    jQuery('.foxholder-form__input--effect-3, .foxholder-form__textarea--effect-3').blur(function() {
-      if (! jQuery(this).hasClass('is-active')) {
-        jQuery(this).css({'padding-left': 20});
-      } 
+      jQuery(this).focus(function() {
+        jQuery(this).css({'padding-left': labelWidth});
+      });
+
+      jQuery(this).blur(function() {
+        if (! jQuery(this).hasClass('is-active')) {
+          jQuery(this).css({'padding-left': initPadding});
+        } 
+      });
+      
     });
   }
 
   if (number.demo == 4) {
 
     //example-4 moving to the left
-    jQuery('.foxholder-form__input--effect-4, .foxholder-form__textarea--effect-4').focus(function() {
+    jQuery('.foxholder-form__input--effect-4, .foxholder-form__textarea--effect-4').each(function() {
 
-      var labelWidth;
+      var labelWidth, initLeft;
       labelWidth = jQuery(this).next('label').width();
-      console.log(labelWidth);
-      jQuery(this).next('label').css({'left': 0 - (labelWidth + 30)});
-    });
+      initLeft = jQuery(this).next('label').css('left');
+      console.log(initLeft);
+      
+      jQuery(this).focus(function() {
+        jQuery(this).next('label').css({'left': - (labelWidth + 30)});
+      });
 
-    jQuery('.foxholder-form__input--effect-4, .foxholder-form__textarea--effect-4').blur(function() {
-      if (! jQuery(this).hasClass('is-active')) {
-        jQuery(this).next('label').css({'left': 1});
-      } 
+      jQuery(this).blur(function() {
+        if (! jQuery(this).hasClass('is-active')) {
+          jQuery(this).next('label').css({'left': initLeft});
+        } 
+      });
     });
-
   }
 
   if (number.demo == 7) {
@@ -109,21 +101,22 @@ jQuery.fn.foxholder = function(number) {
     jQuery('.foxholder-form__input--effect-9, .foxholder-form__textarea--effect-9').each(function() {
       jQuery(this).parent().append('<div class="foxholder-form__overlay foxholder-form__overlay--effect-9"></div>');
 
-      var labelWidth, labelHeight;
+      var labelWidth, labelHeight, initPadding;
       labelWidth = jQuery(this).siblings('label').width();
       labelHeight = jQuery(this).siblings('label').height();
+      initPadding = jQuery(this).css('padding-left');
 
       if (jQuery(this).is('.foxholder-form__input')) {
         jQuery(this).siblings('.foxholder-form__overlay--effect-9').css({
           'width': labelWidth,
           'height': '100%',
-          'left': 0 - (labelWidth + 40),
+          'left': -(labelWidth + 40),
         });
       } else {
         jQuery(this).siblings('.foxholder-form__overlay--effect-9').css({
           'width': labelWidth, 
           'height' : labelHeight,
-          'left': 0 - (labelWidth + 40),
+          'left': - (labelWidth + 40),
         });
       }
 
@@ -133,7 +126,7 @@ jQuery.fn.foxholder = function(number) {
 
       jQuery(this).blur(function() {
         if (!jQuery(this).hasClass('is-active')) {
-          jQuery(this).css({'padding-left': 20});
+          jQuery(this).css({'padding-left': initPadding});
         }
       });
     });
@@ -143,19 +136,21 @@ jQuery.fn.foxholder = function(number) {
   if (number.demo == 10) {
 
     //example-10 label top position
-    jQuery('.foxholder-form__input--effect-10, .foxholder-form__textarea--effect-10').focus(function() {
-      var labelTop;
+    jQuery('.foxholder-form__input--effect-10, .foxholder-form__textarea--effect-10').each(function() {
+      var labelTop, initTop;
       labelTop = parseInt(jQuery(this).css('padding-top'));
-      jQuery(this).next('label').css({'top': 0 - (labelTop + 12)});
-      console.log(labelTop);
-    });
+      initTop = parseInt(jQuery(this).siblings('label').css('top'));
+      
+      jQuery(this).focus(function() {
+        jQuery(this).next('label').css({'top': - (labelTop + 12)});
+      });
 
-    jQuery('.foxholder-form__input--effect-10, .foxholder-form__textarea--effect-10').blur(function() {
-      if (! jQuery(this).hasClass('is-active')) {
-        jQuery(this).next('label').css({'top': 0});
-      }
+      jQuery(this).blur(function() {
+        if (! jQuery(this).hasClass('is-active')) {
+          jQuery(this).next('label').css({'top': initTop});
+        }
+      });
     });
-
   }
 
   if (number.demo == 11) {
@@ -170,21 +165,23 @@ jQuery.fn.foxholder = function(number) {
   if (number.demo == 13) {
 
     //example-13 elements padding
-    jQuery('.foxholder-form__input--effect-13, .foxholder-form__textarea--effect-13').focus(function() {
-      var labelWidth;
+    jQuery('.foxholder-form__input--effect-13, .foxholder-form__textarea--effect-13').each(function() {
+      var labelWidth, initPadding;
       labelWidth = jQuery(this).siblings('label').width() + 54;
+      initPadding = jQuery(this).css('padding-left');
       
-      if (! jQuery(this).hasClass('is-active')) {
-        jQuery(this).css({'padding-left': labelWidth});
-      }
-    });
+      jQuery(this).focus(function() {
+        if (! jQuery(this).hasClass('is-active')) {
+          jQuery(this).css({'padding-left': labelWidth});
+        } 
+      });
 
-    jQuery('.foxholder-form__input--effect-13, .foxholder-form__textarea--effect-13').blur(function() {
-      if (jQuery(this).hasClass('is-active')) {
-        jQuery(this).css({'padding-left': 20});
-      } 
+      jQuery(this).blur(function() {
+        if (! jQuery(this).hasClass('is-active')) {
+          jQuery(this).css({'padding-left': initPadding});
+        } 
+      });
     });
-
   }
 
   if (number.demo == 14) {
@@ -195,39 +192,42 @@ jQuery.fn.foxholder = function(number) {
     });
 
     //example-14 elements padding
-    jQuery('.foxholder-form__input--effect-14, .foxholder-form__textarea--effect-14').focus(function() {
-      var labelWidth;
+    jQuery('.foxholder-form__input--effect-14, .foxholder-form__textarea--effect-14').each(function() {
+      var labelWidth, initPadding;
       labelWidth = jQuery(this).siblings('label').width() + 20;
-      jQuery(this).css({'padding-left': labelWidth});
-    });
+      initPadding = jQuery(this).css('padding-left');
+      
+      jQuery(this).focus(function() {
+        jQuery(this).css({'padding-left': labelWidth});
+      });
 
-    jQuery('.foxholder-form__input--effect-14, .foxholder-form__textarea--effect-14').blur(function() {
-      if (! jQuery(this).hasClass('is-active')) {
-        jQuery(this).css({'padding-left': 20});
-      } 
+      jQuery(this).blur(function() {
+        if (! jQuery(this).hasClass('is-active')) {
+          jQuery(this).css({'padding-left': initPadding});
+        } 
+      });
+
     });    
 
   }
 
-
   if (number.demo == 15) {
 
-    //example-15 adding triangle icons
-    jQuery('.foxholder-form__input--effect-15, .foxholder-form__textarea--effect-15').each(function() {
-      jQuery(this).next('label').append('<div class="foxholder-form__helper foxholder-form__helper--top"></div>').append('<div class="foxholder-form__helper foxholder-form__helper--bottom"></div>');
-    });
-
     //example-15 elements padding
-    jQuery('.foxholder-form__input--effect-15, .foxholder-form__textarea--effect-15').focus(function() {
-      var labelWidth;
+    jQuery('.foxholder-form__input--effect-15, .foxholder-form__textarea--effect-15').each(function() {
+      var labelWidth, initPadding;
       labelWidth = jQuery(this).siblings('label').width() + 36;
-      jQuery(this).css({'padding-left': labelWidth});
-    });
+      initPadding = jQuery(this).css('padding-left');
+      
+      jQuery(this).focus(function() {
+        jQuery(this).css({'padding-left': labelWidth});
+      });
 
-    jQuery('.foxholder-form__input--effect-15, .foxholder-form__textarea--effect-15').blur(function() {
-      if (! jQuery(this).hasClass('is-active')) {
-        jQuery(this).css({'padding-left': 20});
-      } 
+      jQuery(this).blur(function() {
+        if (! jQuery(this).hasClass('is-active')) {
+          jQuery(this).css({'padding-left': initPadding});
+        } 
+      });
     });
     
   }
